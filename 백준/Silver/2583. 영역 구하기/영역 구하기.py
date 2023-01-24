874ebcc95dd461f94943226eab7fb3129dc43aa0
pandorasususu <97590919+pandorasususu@users.noreply.github.com>
2023-01-24 14:09:02 +0900

    [Silver I] Title: 영역 구하기, Time: 52 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,36 +1,43 @@
 import sys
 input = sys.stdin.readline
+def dfs(i,j,m,n,arr):
+    s = [(i,j)]
+    dx,dy = (0,0,1,-1),(1,-1,0,0)
+    cnt = 1
+    arr[i][j] = True
+    while s:
+        x, y = s.pop()
+        for dir in range(4):
+            nx, ny = x + dx[dir], y + dy[dir]
+            if nx < 0 or nx >= m or ny < 0 or ny >= n:
+                continue
+            if arr[nx][ny]:
+                continue
+            cnt += 1
+            s.append((nx,ny))
+            arr[nx][ny] = True
+    return cnt
 
-def sol(stack):
-    global arr, area
-    while stack:
-        sr, sc = stack.pop()
-        moves = [[0,1],[0,-1],[1,0],[-1,0]]
-        for move in moves:
-            nr, nc = sr + move[0], sc + move[1]
-            if 0 <= nr and nr < n and 0 <= nc and nc < m and arr[nr][nc] == False:
-                arr[nr][nc] = True
-                area += 1
-                stack.append([nr, nc])
-    return area
+def sol():
+    m,n,k = map(int, input().split())
+    # 빈공간이면 False 직사각형이 있으면 True
+    arr = [[False]*n for _ in range(m)]
+    # 직사각형에 해당하는 범위 체크
+    for _ in range(k):
+        x1,y1,x2,y2 = map(int,input().split())
+        for i in range(y1,y2):
+            for j in range(x1,x2):
+                arr[i][j] = True
     
-n, m, k = map(int, input().split())
-arr = [[False]*m for _ in range(n)]
+    # dfs를 통해 분리된 영역 넓이 구하기
+    cnt = []
+    for i in range(m):
+        for j in range(n):
+            if not arr[i][j]:
+                cnt.append(dfs(i,j,m,n,arr))
+    cnt.sort()
+    print(len(cnt))
+    print(*cnt)
+    return 
 
-for _ in range(k):
-    x1, y1, x2, y2 = map(int, input().split())
-    for c in range(x1, x2):
-        for r in range(y1, y2):
-            arr[r][c] = True
-
-
-area_list = []
-for r in range(n):
-    for c in range(m):
-        if arr[r][c] == False:
-            arr[r][c] = True
-            area = 1
-            area_list.append(sol([[r, c]]))
-area_list.sort()
-print(len(area_list))
-print(*area_list)
+sol()
