d910a78c1ee25180536f6db8bbc08f8192e866eb
pandorasususu <97590919+pandorasususu@users.noreply.github.com>
2023-01-24 14:04:19 +0900

    [Silver I] Title: 영역 구하기, Time: 124 ms, Memory: 115136 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,7 +1,5 @@
 import sys
-from pprint import pprint
 input = sys.stdin.readline
-# sys.setrecursionlimit(10**6)
 
 def sol(stack):
     global arr, area
@@ -10,29 +8,27 @@ def sol(stack):
         moves = [[0,1],[0,-1],[1,0],[-1,0]]
         for move in moves:
             nr, nc = sr + move[0], sc + move[1]
-            # if 0 <= nr and nr < n and 0 <= nc and nc < m and arr[nr][nc] == 0 and visited[nr][nc] == 0:
-            if 0 <= nr and nr < n and 0 <= nc and nc < m and arr[nr][nc] == 0:
-                arr[nr][nc] = 1
+            if 0 <= nr and nr < n and 0 <= nc and nc < m and arr[nr][nc] == False:
+                arr[nr][nc] = True
                 area += 1
                 stack.append([nr, nc])
     return area
     
 n, m, k = map(int, input().split())
-arr = [[0]*m for _ in range(n)]
-visited = [[0]*m for _ in range(n)]
+arr = [[False]*m for _ in range(n)]
 
 for _ in range(k):
     x1, y1, x2, y2 = map(int, input().split())
     for c in range(x1, x2):
         for r in range(y1, y2):
-            arr[r][c] = 1
+            arr[r][c] = True
 
 
 area_list = []
 for r in range(n):
     for c in range(m):
-        if arr[r][c] == 0 and visited[r][c] == 0:
-            arr[r][c] = 1
+        if arr[r][c] == False:
+            arr[r][c] = True
             area = 1
             area_list.append(sol([[r, c]]))
 area_list.sort()
