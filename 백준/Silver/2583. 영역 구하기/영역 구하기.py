3364acff64db2ff4b00e9804db478e0281c85060
pandorasususu <97590919+pandorasususu@users.noreply.github.com>
2023-01-24 13:59:38 +0900

    [Silver I] Title: 영역 구하기, Time: 308 ms, Memory: 39828 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -3,6 +3,26 @@ from pprint import pprint
 input = sys.stdin.readline
 # sys.setrecursionlimit(10**6)
 
+def sol(stack):
+    global visited, area
+    while stack:
+        sr, sc = stack.pop()
+        moves = [[0,1],[0,-1],[1,0],[-1,0]]
+        for move in moves:
+            nr, nc = sr + move[0], sc + move[1]
+            if 0 <= nr and nr < n and 0 <= nc and nc < m and arr[nr][nc] == 0 and visited[nr][nc] == 0:
+                visited[nr][nc] = 1
+                area += 1
+                stack.append([nr, nc])
+            # if nr < 0 or nr >= n or nc < 0 or nc >= m:
+            #     continue 
+            # if arr[nr][nc] != 0 or visited[nr][nc] != 0:
+            #     continue
+            # visited[nr][nc] = 1
+            # area += 1
+            # stack.append([nr, nc])
+    return area
+    
 n, m, k = map(int, input().split())
 arr = [[0]*m for _ in range(n)]
 visited = [[0]*m for _ in range(n)]
@@ -13,19 +33,6 @@ for _ in range(k):
         for r in range(y1, y2):
             arr[r][c] = 1
 
-def sol(stack):
-    global visited, area
-    while stack:
-        start = stack.pop()
-        sr, sc = start[0], start[1]
-        moves = [[0,1],[0,-1],[1,0],[-1,0]]
-        for move in moves:
-            nr, nc = sr + move[0], sc + move[1]
-            if 0 <= nr and nr < n and 0 <= nc and nc < m and arr[nr][nc] == 0 and visited[nr][nc] == 0:
-                visited[nr][nc] = 1
-                area += 1
-                stack.append([nr, nc])
-    return area
 
 area_list = []
 for r in range(n):
