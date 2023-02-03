b2595c039bd105d73555f868da8997b2a66a931d
pandorasususu <97590919+pandorasususu@users.noreply.github.com>
2023-02-03 14:37:54 +0900

    [Gold V] Title: 평범한 배낭, Time: 148 ms, Memory: 115112 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -4,13 +4,11 @@ input = stdin.readline
 
 n, k = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
-knapsack = [[0] * (k+1) for _ in range(n+1)]
-for i in range(n):
-    for j in range(1, k+1):
-        w, v = arr[i][0], arr[i][1]
+arr.sort()
+dp = [0] * (k+1)
 
-        if j < w:
-            knapsack[i][j] = knapsack[i-1][j]
-        else:
-            knapsack[i][j] = max(v + knapsack[i-1][j-w], knapsack[i-1][j])
-print(knapsack[n-1][k])
+for w, v in arr:
+    for i in range(k, w-1, -1):
+        dp[i] = max(dp[i-w] + v, dp[i])
+
+print(dp[k])
