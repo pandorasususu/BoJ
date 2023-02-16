b3fe3f7e112614ae0818e82c4618fd746429b7f8
pandorasususu <97590919+pandorasususu@users.noreply.github.com>
2023-02-16 14:51:46 +0900

    [Gold V] Title: 최소비용 구하기, Time: 304 ms, Memory: 57508 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,30 @@
+from sys import stdin
+import heapq
+input = stdin.readline
+
+            
+n, m = int(input()), int(input())
+graph = [[] * (n+1) for _ in range(n+1)]
+for _ in range(m):
+    start, end, weight = map(int, input().split())
+    graph[start].append((end, weight))
+s, e = map(int, input().split())
+distance = [float('inf')] * (n+1)
+def sol(k):
+    q = []
+    heapq.heappush(q, (s, 0))
+    distance[s] = 0
+
+    while q:
+        now, dist = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        else:
+            for i in graph[now]:
+                cnd = dist + i[1]
+                if cnd < distance[i[0]]:
+                    distance[i[0]] = cnd
+                    heapq.heappush(q, (i[0], cnd))
+
+sol(s)
+print(distance[e])
