2ea0c78bebc7b60f05a8aa35c9b3015ceafab860
pandorasususu <97590919+pandorasususu@users.noreply.github.com>
2023-02-20 12:54:58 +0900

    [Gold V] Title: 동전 1, Time: 200 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -11,6 +11,6 @@ dp = [0] * (k+1)
 dp[0] = 1
 for i in arr:
     for j in range(i, k+1):
-        if j - i >= 0:
-            dp[j] += dp[j-i]
+        # if j - i >= 0:
+        dp[j] += dp[j-i]
 print(dp[k])
