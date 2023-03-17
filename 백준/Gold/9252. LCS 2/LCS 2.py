9955f889d7d0df37d9d7065624d4f2f40e530d61
이병헌 <97590919+pandorasususu@users.noreply.github.com>
2023-03-17 15:18:12 +0900

    [Gold IV] Title: LCS 2, Time: 352 ms, Memory: 55728 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.py
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -1,39 +1,29 @@
 import sys
-# from collections import deque
-# import random
 input = sys.stdin.readline
 
-one, two = input().rstrip(), input().rstrip()
+first_word, second_word = input(), input()
+fw_len, sw_len = len(first_word), len(second_word)
 
-def LCS(X, Y):
-    m = len(X)
-    n = len(Y)
-
-    dp = [[0] * (n + 1) for _ in range(m + 1)]
-
-    # dp 배열 채우기
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
-            if X[i - 1] == Y[j - 1]:
+def sol():
+    dp = [[0] * (sw_len + 1) for _ in range(fw_len + 1)]
+    for i in range(1, fw_len + 1):
+        for j in range(1, sw_len + 1):
+            if first_word[i - 1] == second_word[j - 1]:
                 dp[i][j] = dp[i - 1][j - 1] + 1
             else:
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
-    # LCS 구하기
-    lcs = ""
-    i, j = m, n
+    ans = ''
+    i, j = fw_len, sw_len
     while i > 0 and j > 0:
-        if X[i - 1] == Y[j - 1]:
-            lcs = X[i - 1] + lcs
+        if first_word[i - 1] == second_word[j - 1]:
+            ans = first_word[i - 1] + ans
             i -= 1
             j -= 1
         elif dp[i - 1][j] > dp[i][j - 1]:
             i -= 1
         else:
             j -= 1
-
-    return dp[-1][-1], lcs
-
-
-print(LCS(one, two)[0])
-print(LCS(one, two)[1])
+    print(dp[fw_len - 1][sw_len - 1])
+    print(ans)
+sol()
