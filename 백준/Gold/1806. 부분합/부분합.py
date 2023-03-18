6559ef29dc552573352c1e98f5eda3e8f9c18ed6
이병헌 <97590919+pandorasususu@users.noreply.github.com>
2023-03-18 20:14:18 +0900

    [Gold IV] Title: 부분합, Time: 120 ms, Memory: 42340 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,24 +1,24 @@
+import sys
+
+input = sys.stdin.readline
 n, s = map(int, input().split())
 arr = list(map(int, input().split()))
 
 left, right = 0, 0
-min_length = n + 1
+min_len = n + 1
 sum = arr[0]
-
 while left <= right and right < n:
     if sum < s:
         right += 1
         if right < n:
             sum += arr[right]
-
     else:
-        length = right - left + 1
-        if length < min_length:
-            min_length = length
+        cur_len = right - left + 1
+        if cur_len < min_len:
+            min_len = cur_len
         sum -= arr[left]
         left += 1
-
-if min_length == n + 1:
+if min_len == n + 1:
     print(0)
 else:
-    print(min_length)
+    print(min_len)
