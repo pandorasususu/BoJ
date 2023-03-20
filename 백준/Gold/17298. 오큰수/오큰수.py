5c4ab875c40fc394b9aa5f66fd3c1e4e66c69156
이병헌 <97590919+pandorasususu@users.noreply.github.com>
2023-03-20 18:56:25 +0900

    [Gold IV] Title: 오큰수, Time: 912 ms, Memory: 155568 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -8,11 +8,11 @@ def sol():
     stack = []
     ans = [-1] * n
     for i in range(len(arr)-1, -1, -1):
-        while stack and arr[i] >= stack[-1][0]:
+        while stack and arr[i] >= stack[-1]:
             stack.pop()
         if stack:
-            ans[i] = stack[-1][0] 
-        stack.append((arr[i], i))
+            ans[i] = stack[-1] 
+        stack.append(arr[i])
     return ans
 
 print(*sol())
